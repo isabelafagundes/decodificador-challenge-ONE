f5c689320a223b1f9f2097d67a21c5766d2843f7
Isabela <isabelahidalgo.2004@gmail.com>
2023-01-08 22:17:52 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const btnCripto = document.querySelector('#btn-cripto');
+const btnDescri = document.querySelector('#btn-descri');
+const exibirMensagem = document.querySelector('#exibir-msg');
+const btnCopy = document.querySelector('#copy');
+const texto = document.querySelector('#texto');
+const imagem = document.querySelector('.imagem');
+
+btnCripto.addEventListener('click', function(e){
+    
+    e.preventDefault();
+
+    const mensagem = document.querySelector('#input');
+    let mensagemValue = mensagem.value;
+    
+    if(mensagemValue == ''){
+        alert("Digite um texto!");
+        console.log(textoExibidoValue);
+    }else if(!(mensagemValue == '')){
+        criptografar(mensagemValue);
+    }
+
+});
+
+btnDescri.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    const mensagem = document.querySelector('#input');
+    let mensagemValue = mensagem.value;
+    let textoExibidoValue = document.querySelector('#exibir-msg').innerHTML;
+    
+    if(mensagemValue == ''){
+        alert("Digite um texto!");
+        console.log(textoExibidoValue);
+    }else if(!(mensagemValue == '')){
+        descriptografar(mensagemValue);
+        descriptografar(textoExibidoValue);
+    }
+
+});
+
+function botaoCopiar(){
+
+    navigator.clipboard.writeText(exibirMensagem.value);
+    alert('Texto copiado para a área de transferência !');
+    
+}
+
+
+function criptografar(vogal){
+
+        if(vogal.includes('e') || vogal.includes('i') || vogal.includes('a') || vogal.includes('o') || vogal.includes('u')){
+            vogal = vogal.replaceAll('e', 'enter');
+            vogal = vogal.replaceAll('i', 'imes');
+            vogal = vogal.replaceAll('a', 'ai');
+            vogal = vogal.replaceAll('o', 'ober');
+            vogal = vogal.replaceAll('u', 'ufat');
+            exibirMensagem.innerHTML = vogal;
+            texto.innerHTML = '';
+            mensagemValue = vogal;
+            imagem.style.display = 'none';
+            btnCopy.style.display = 'flex';
+    }
+ 
+}
+
+function descriptografar(palavra){
+
+    if(palavra.includes('enter') || palavra.includes('imes') || palavra.includes('ai') || palavra.includes('ober') || palavra.includes('ufat')){
+        palavra = palavra.replaceAll('enter', 'e');
+        palavra = palavra.replaceAll('imes', 'i');
+        palavra = palavra.replaceAll('ai', 'a');
+        palavra = palavra.replaceAll('ober', 'o');
+        palavra = palavra.replaceAll('ufat', 'u');
+        exibirMensagem.innerHTML = palavra;
+        texto.innerHTML = '';
+        imagem.style.display = 'none';
+        mensagemValue = palavra;
+    }
+
+}
